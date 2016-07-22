81cd9f46f1050b8766a547eca14a15194b2e39c3
Francisco Nunes <maxnevesnunes@gmail.com>
2016-07-22 11:36:53 +0100

    Implemented splits without layout tracking

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,11 +1,29 @@
-angular.module('app',[])
-  .controller('layoutCtrl', ['$scope', function($scope){
-    
+var layoutApp = angular.module('layoutApp',[]);
+
+layoutApp.controller('layoutCtrl', ['$scope', function($scope){
     $scope.Types = [{}];
     $scope.width = 12;
     
-    $scope.add = function(){
+    $scope.addColumn = function(){
       $scope.Types.push({});
       $scope.width = $scope.Types.length < 7 ? Math.floor(12/$scope.Types.length) : 1;
     };
-  }]);
+}]);
+
+layoutApp.directive("splitvertical", function($compile){
+	return function(scope, element, attrs){
+		element.bind("click", function(){
+			scope.count++;
+			angular
+                .element(
+                    document.getElementById('layout-rows'))
+                .append($compile(
+                    "<div class='row'>" +
+                    "<div class='col-md-8'>" +
+                    "<div class='layout-view'>.col-md-8</div>" +
+                    "</div>" +
+                    "</div>"
+                )(scope));
+		});
+	};
+});
