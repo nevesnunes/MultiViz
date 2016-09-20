fe6378d8fab13b8c734cde57086f8a4c809031ec
Francisco Nunes <maxnevesnunes@gmail.com>
2016-09-20 11:01:02 +0100

    Added grunt task for Angular module graph

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+	angular_architecture_graph: {
+		diagram: {
+			files: {
+				"architecture": [
+					"scripts/*.js"
+				]
+			}
+		}
+	}
+  });
+
+  grunt.loadNpmTasks('grunt-angular-architecture-graph');
+
+  grunt.registerTask('default', ['angular_architecture_graph']);
+};
